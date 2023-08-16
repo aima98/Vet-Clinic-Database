2ed6fb9e2ae49ecfb3c6e53e93c96471caf2bdbc
Oumaima Nejjari <oumanj1998@gmail.com>
2023-08-16 13:04:48 +0100

    Adding transactions to queries file

--- queries.sql
+++ queries.sql
@@ -7,3 +7,51 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM  animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- Setting species column to unspecified
+-- Verifying changes and roll back the change
+-- Verifying that species went back to previous state
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT save_point1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO save_point1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+-- Animals' Number
+SELECT COUNT(*) FROM animals;
+
+-- Animals that never tried to escape
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- Average weight of animals
+SELECT AVG(weight_kg) FROM animals;
+
+-- Animals that escapes the most,  the ones neutered or not
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+-- The minimum and maximum weight of each type of animal
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+-- The average number of escape attempts per animal type of those born between 1990 and 2000
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;

c8fe545889132a72a12e3a640932c96ac8463734
Oumaima Nejjari <oumanj1998@gmail.com>
2023-08-16 16:21:53 +0100

    Craeting vets, Specializations and Visists tables

--- schema.sql
+++ schema.sql
@@ -37,3 +37,21 @@ ALTER TABLE animals
 ADD CONSTRAINT owners_fk
 FOREIGN KEY(owner_id) 
 REFERENCES owners(id); 
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES species(id),
+    vet_id INT REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animals_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    date_of_visit DATE
+);
